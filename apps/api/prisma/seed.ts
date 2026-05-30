6dd765131377dcfbde35fe2933d3535fdb054340
SyK5 <Alpay.Sahin@outlook.de>
2026-05-30 16:40:07 +0200

    chore: Minor cleanup and formatting

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -9,15 +9,14 @@ async function main() {
     { slug: 'valorant', name: 'Valorant' },
     { slug: 'lol', name: 'League of Legends' },
     { slug: 'dota2', name: 'Dota 2' },
-    { slug: 'pubg', name: 'PlayerUnknown\'s Battlegrounds' },
+    { slug: 'pubg', name: "PlayerUnknown's Battlegrounds" },
     { slug: 'fortnite', name: 'Fortnite' },
     { slug: 'minecraft', name: 'Minecraft' },
     { slug: 'minecraft-java', name: 'Minecraft Java Edition' },
-    { slug: 'cod', name: 'Call of Duty: Modern Warfare'},
-    { slug: 'bo7', name: 'Call of Duty: Black Ops 7'},
-    { slug: 'bo6', name: 'Call of Duty: Black Ops 6'},
-    { slug: 'gta5', name: 'Grand Theft Auto V'},
-
+    { slug: 'cod', name: 'Call of Duty: Modern Warfare' },
+    { slug: 'bo7', name: 'Call of Duty: Black Ops 7' },
+    { slug: 'bo6', name: 'Call of Duty: Black Ops 6' },
+    { slug: 'gta5', name: 'Grand Theft Auto V' },
   ]
 
   for (const game of games) {
