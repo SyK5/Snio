6fc2b1a0d8eda22c96f4a38c9de459584abe6aca
SyK5 <Alpay.Sahin@outlook.de>
2026-05-30 16:40:08 +0200

    refactor(api): Setup global Prisma module

---
+++ apps/api/src/common/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({ omit: { user: { password_hash: true } } })
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect()
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect()
+  }
+}
